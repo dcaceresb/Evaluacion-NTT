83f792bfc2cc8a76296107202b17ebfd3bcec70c
David <dcacerbe@outlook.com>
2023-08-25 22:17:24 -0400

    initial commit

---
+++ src/main/java/com/dcaceresb/ntt_test/NttTestApplication.java
@@ -0,0 +1,13 @@
+package com.dcaceresb.ntt_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcaceresb/ntt_test/NttTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcaceresb.ntt_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
